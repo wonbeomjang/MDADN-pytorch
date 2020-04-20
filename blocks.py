4812037f40b95a74535f252770d2c638d0a16ff6
wonbeomjang <jtiger958@gmail.com>
2020-04-21 02:42:44 +0900

    correct error

--- blocks.py
+++ blocks.py
@@ -5,15 +5,14 @@ import torch
 class AttentionModule(nn.Module):
     def __init__(self, in_channels):
         super(AttentionModule, self).__init__()
-
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.max_pool = nn.AdaptiveMaxPool2d(1)
         self.channel_attention_avg = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
             nn.Linear(in_channels, in_channels // 8),
             nn.Linear(in_channels // 8, in_channels // 8),
             nn.Linear(in_channels // 8, in_channels)
         )
         self.channel_attention_max = nn.Sequential(
-            nn.AdaptiveMaxPool2d(1),
             nn.Linear(in_channels, in_channels // 8),
             nn.Linear(in_channels // 8, in_channels // 8),
             nn.Linear(in_channels // 8, in_channels)
@@ -26,11 +25,12 @@ class AttentionModule(nn.Module):
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         identity = x
-
-        channel_attention = nn.functional.sigmoid(self.channel_attention_avg(x) + self.channel_attention_max(x))
+        channel_attention_avg = self.avg_pool(x).flatten(start_dim=1)
+        channel_attention_max = self.max_pool(x).flatten(start_dim=1)
+        channel_attention: torch.Tensor = nn.functional.sigmoid(self.channel_attention_avg(channel_attention_avg) + self.channel_attention_max(channel_attention_max))
+        channel_attention = channel_attention.unsqueeze(dim=-1).unsqueeze(dim=-1)
         x *= channel_attention
-
-        spatial_attention = torch.cat([x.max(dim=1, keepdim=True), x.mean(dim=1, keepdim=True)], dim=1)
+        spatial_attention = torch.cat([x.max(dim=1, keepdim=True)[0], x.mean(dim=1, keepdim=True)], dim=1)
         spatial_attention = self.spatial_attention(spatial_attention)
 
         x *= spatial_attention
@@ -67,6 +67,7 @@ class ResidualBox(nn.Module):
 
         residual = []
 
+        in_channels = out_channels
         for channel in residual_cfg:
             residual += conv_block(in_channels, channel)
             in_channels = channel
--- models.py
+++ models.py
@@ -7,26 +7,37 @@ from blocks import attention_block, conv_block, ResidualBox
 class MDADN(nn.Module):
     def __init__(self, num_object):
         super(MDADN, self).__init__()
-        self.block_1 = nn.Sequential(*attention_block(3, 32))
+        self.block_1 = nn.Sequential(*attention_block(5, 32))
         self.block_2 = nn.Sequential(*attention_block(32, 64))
         self.block_3 = nn.Sequential(ResidualBox(64, 128, [64]))
         self.block_4 = nn.Sequential(ResidualBox(128, 256, [128]))
         self.block_5 = nn.Sequential(ResidualBox(256, 512, [256, 512, 256]))
         self.block_6 = nn.Sequential(ResidualBox(512, 1024, [512, 1024, 512, 1024, 1024]))
         self.block_7 = nn.Sequential(*conv_block(1024 + 64, 1024))
-        self.block_8 = nn.Conv2d(1924, num_object, 3, padding=1)
-        self.slip_connection = nn.Sequential(*conv_block(256, 64, 1))
+        self.block_8 = nn.Conv2d(1024, num_object, 3, padding=1)
+        self.skip_connection = nn.Sequential(*conv_block(256, 64, 1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.block_1(x)
         x = self.block_2(x)
         x = self.block_3(x)
-        skip = self.block_4(x)
+        x = self.block_4(x)
+        skip = self.skip_connection(x)
 
-        x = self.block_5(skip)
-        x = torch.cat([self.block_6(x), skip], dim=1)
+        x = self.block_5(x)
+        x = self.block_6(x)
+        x = torch.cat([x, skip], dim=1)
 
         x = self.block_7(x)
         x = self.block_8(x)
 
         return x
+
+
+if __name__ == '__main__':
+    with torch.no_grad():
+        model = MDADN(3).cuda()
+        for i in range(100):
+            image = torch.ones([1, 5, 224, 224]).cuda()
+            image = model(image)
+            print(image.shape)
