6d3af41dc436e7f81006b0391d555fda933fbf75
Jang Wonbeom <jtiger958@gmail.com>
2020-04-26 23:41:08 +0900

    add maxpool

--- models.py
+++ models.py
@@ -7,14 +7,15 @@ from blocks import attention_block, conv_block, ResidualBox
 class MDADN(nn.Module):
     def __init__(self, num_object):
         super(MDADN, self).__init__()
-        self.block_1 = nn.Sequential(*attention_block(5, 32))
-        self.block_2 = nn.Sequential(*attention_block(32, 64))
-        self.block_3 = nn.Sequential(ResidualBox(64, 128, [64]))
-        self.block_4 = nn.Sequential(ResidualBox(128, 256, [128]))
-        self.block_5 = nn.Sequential(ResidualBox(256, 512, [256, 512, 256]))
+        self.block_1 = nn.Sequential(*attention_block(5, 32), nn.MaxPool2d(2))
+        self.block_2 = nn.Sequential(*attention_block(32, 64), nn.MaxPool2d(2))
+        self.block_3 = nn.Sequential(ResidualBox(64, 128, [64]), nn.MaxPool2d(2))
+        self.block_4 = nn.Sequential(ResidualBox(128, 256, [128]), nn.MaxPool2d(2))
+        self.block_5 = nn.Sequential(ResidualBox(256, 512, [256, 512, 256]), nn.MaxPool2d(2))
         self.block_6 = nn.Sequential(ResidualBox(512, 1024, [512, 1024, 512, 1024, 1024]))
         self.block_7 = nn.Sequential(*conv_block(1024 + 64, 1024))
         self.block_8 = nn.Conv2d(1024, num_object, 3, padding=1)
+
         self.skip_connection = nn.Sequential(*conv_block(256, 64, 1))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -23,6 +24,11 @@ class MDADN(nn.Module):
         x = self.block_3(x)
         x = self.block_4(x)
         skip = self.skip_connection(x)
+
+        batch_size, num_channel, height, width = skip.data.size()
+        skip = skip.view(batch_size, int(num_channel / 4), height, 2, width, 2).contiguous()
+        skip = skip.permute(0, 3, 5, 1, 2, 4).contiguous()
+        skip = skip.view(batch_size, -1, int(height / 2), int(width / 2))
 
         x = self.block_5(x)
         x = self.block_6(x)
